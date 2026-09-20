96cd94dd4a721f4a98ca374aa68191f49142e602
woochan-jeon <vvch120@hufs.ac.kr>
2026-09-20 16:42:31 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // - Storage 정적 웹사이트 + 별도 Function App 구조에서는 Function App 주소를 넣습니다.
 //   예)  window.API_BASE = 'https://내함수앱이름.azurewebsites.net';
 //   (끝에 / 를 붙이지 않아도 됩니다)
-window.API_BASE = '';
+window.API_BASE = 'https://skyline-api-woochan-bwc5braddsapbwhc.koreacentral-01.azurewebsites.net';
